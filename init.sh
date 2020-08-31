8fbb08a247366781c587254a66cb5c36399b4c38
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-08-31 15:54:59 +0100

    [add] nvm install

--- init.sh
+++ init.sh
@@ -1,7 +1,15 @@
-# for gpio command
-sudo apt install wiringpi
-# install pip3
-sudo apt install python3-pip
+# # for gpio command
+# sudo apt install wiringpi
+# # install pip3
+# sudo apt install python3-pip
+# 
+# # install python package using pip3
+# pip3 install -r requirements.txt
 
-# install python package using pip3
-pip3 install -r requirements.txt
+# install nvm
+sudo mkdir /usr/local/nvm
+sudo chmod 777 /usr/local/nvm
+git clone https://github.com/creationix/nvm.git /usr/local/nvm
+source /usr/local/nvm/nvm.sh
+nvm install v12.18.3
+
