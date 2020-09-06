a5ccd20eee6b4bede5b5ee9ad1751880c251deb0
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-09-06 05:56:18 +0100

    [add] generate config file logic

--- init.sh
+++ init.sh
@@ -11,3 +11,6 @@ curl http://abyz.me.uk/rpi/pigpio/code/irrp_py.zip | zcat > irrp.py
 
 # pip install
 pip3 install -r requirements.txt
+
+# generate config file
+cp config.json.template config.json
